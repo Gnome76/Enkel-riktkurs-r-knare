0c478fe19a4bc1d962360cb77bdc7d51b032e357
Gnome76 <magber542@gmail.com>
2025-06-16 19:37:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-st.write(os.listdir("."))  # Visa lista över filer i rotmappen
 import streamlit as st
 from data_handler import load_data
 from forms import lagg_till_bolag_form, redigera_bolag_form, tabort_bolag_form
