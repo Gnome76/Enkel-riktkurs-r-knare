9f7d10b058c8317fe0de1e65cf97a74815b00798
Gnome76 <magber542@gmail.com>
2025-06-16 20:27:11 +0200

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def nytt_bolag_formular(data):
+    st.write("### Test: Formulär laddat")
+
+def redigeringsformular(data):
+    st.write("### Test: Redigeringsformulär laddat")
